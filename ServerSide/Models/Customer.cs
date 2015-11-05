7f2d2aa658308afd89d6e5a25ae9868dc07d3f33
juliusbacosa <juliusbacosa@gmail.com>
2015-11-05 16:01:48 +0800

    - commit everything

---
+++ ServerSide/Models/Customer.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CustomerApiServer.Models
+{
+    public class Customer
+    {
+        [Key]
+        [DatabaseGeneratedAttribute(DatabaseGeneratedOption.Identity)]
+        public int ID { get; set; }
+        public string FullName { get; set; }
+        public string Email { get; set; }     
+    }
+
+    public class CrmDbContext : DbContext
+    {
+        public DbSet<Customer> Customers { get; set; }
+    }
+}
