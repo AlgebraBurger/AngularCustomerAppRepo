e733586ed169ca16bbfd5a5179a5f7871255b05f
juliusbacosa <juliusbacosa@gmail.com>
2015-11-05 18:02:24 +0800

    crud commit

--- ClientSide/Scripts/app/app.js
+++ ClientSide/Scripts/app/app.js
@@ -1,13 +1,19 @@
 ﻿angular.module("CustomerApp", ["ngRoute"]).config(function ($routeProvider) {
     $routeProvider.when("/Home", {
-        templateUrl: "/Views/CustomersList.html"
+        templateUrl: "/Views/CustomersList.html",
+        controller: 'CustomerCtrl'
     });
     $routeProvider.when("/Customers", {
         templateUrl: "/Views/CustomersList.html"
     });
+    $routeProvider.when("/Detail/:CustomerID", {
+        templateUrl: "/Views/CustomerDetail.html",
+        controller: 'CustomerCtrl',
+       
+    });
     $routeProvider.when("/AddCustomer", {
         templateUrl: "/Views/CustomerForm.html"
-    });
+    });  
     $routeProvider.otherwise({
         templateUrl: "/views/CustomersList.html"
     });
--- ClientSide/Scripts/app/controllers/CustomerCtrl.js
+++ ClientSide/Scripts/app/controllers/CustomerCtrl.js
@@ -1,6 +1,5 @@
-﻿angular.module("CustomerApp").controller("CustomerCtrl", function ($scope, $http, $location) {
+﻿angular.module("CustomerApp").controller("CustomerCtrl", function ($scope, $http, $location, $routeParams) {
     $scope.customers = [];
-    
     
     var loadCustomers = function(){
         $http.get("http://localhost:52517/api/customers").success(function (data, status, headers, config) {        
@@ -8,25 +7,35 @@
         
         }).error(function (data, status, headers, config) { });
     }
-
     loadCustomers();
 
-    $scope.CreateCustomer = function (customer) {
+    if ($routeParams.CustomerID) {
+        $http.get("http://localhost:52517/api/customers/" + $routeParams.CustomerID).success(function (data, status, headers, config) {           
+            $scope.customer = data;
+        }).error(function (data, status, headers, config) { });
+    }
+    
+    $scope.updateCustomer = function(customer){
         
-        $http.post('http://localhost:52517/api/Customers',customer).success(function (data) {
-
-            $location.path("Home");
-
+        $http.put("http://localhost:52517/api/customers/" + customer.id, customer).success(function (data) {
             loadCustomers();
+        }).error(function (data) {
+           
+        });
+    }
 
+    $scope.CreateCustomer = function (customer) {        
+        $http.post('http://localhost:52517/api/Customers',customer).success(function (data) {
+            $location.path("Home");
+            loadCustomers();
         }).error(function (error) { 
             console.log(error);
         }).finally(function () {
         	
         });
-
     }
 
+   
     $scope.deleteCustomer = function (customer) {
         $http.delete('http://localhost:52517/api/Customers/' + customer.id).success(function (data) {
 
