6e912c8bf1d811757b2cd118f0667cfbe203e668
juliusbacosa <juliusbacosa@gmail.com>
2015-11-05 15:58:38 +0800

    - commit clients side AngularJs Customer App

---
+++ Scripts/app/app.js
@@ -0,0 +1,19 @@
+﻿angular.module("CustomerApp", ["ngRoute"]).config(function ($routeProvider) {
+    $routeProvider.when("/Home", {
+        templateUrl: "/Views/CustomersList.html"
+    });
+    $routeProvider.when("/Customers", {
+        templateUrl: "/Views/CustomersList.html"
+    });
+    $routeProvider.when("/AddCustomer", {
+        templateUrl: "/Views/CustomerForm.html"
+    });
+    $routeProvider.otherwise({
+        templateUrl: "/views/CustomersList.html"
+    });
+});
+
+
+
+
+
---
+++ Scripts/app/controllers/CustomerCtrl.js
@@ -0,0 +1,45 @@
+﻿angular.module("CustomerApp").controller("CustomerCtrl", function ($scope, $http, $location) {
+    $scope.customers = [];
+    
+    
+    var loadCustomers = function(){
+        $http.get("http://localhost:52517/api/customers").success(function (data, status, headers, config) {        
+            $scope.customers = data;
+        
+        }).error(function (data, status, headers, config) { });
+    }
+
+    loadCustomers();
+
+    $scope.CreateCustomer = function (customer) {
+        
+        $http.post('http://localhost:52517/api/Customers',customer).success(function (data) {
+
+            $location.path("Home");
+
+            loadCustomers();
+
+        }).error(function (error) { 
+            console.log(error);
+        }).finally(function () {
+        	
+        });
+
+    }
+
+    $scope.deleteCustomer = function (customer) {
+        $http.delete('http://localhost:52517/api/Customers/' + customer.id).success(function (data) {
+
+            $.each($scope.customers, function (i) {
+                if ($scope.customers[i].id === customer.id) {
+                    $scope.customers.splice(i, 1);
+                    return false;
+                }
+            });
+
+        }).error(function (data) {
+            
+        });
+    }
+   
+})
